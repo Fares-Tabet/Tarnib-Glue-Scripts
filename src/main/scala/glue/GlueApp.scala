ac9e2f96f44e0d732fa921ce54ec8f0497fbc0c0
Fares-Tabet <44642569+Fares-Tabet@users.noreply.github.com>
2023-08-31 19:37:13 -0400

    added initial logic

--- src/main/scala/glue/GlueApp.scala
+++ src/main/scala/glue/GlueApp.scala
@@ -5,43 +5,58 @@ import com.amazonaws.services.glue.{DataSink, DataSource, DynamicFrame, GlueCont
 import org.apache.spark.{SparkConf, SparkContext}
 import com.amazonaws.services.glue.util.Job
 import com.amazonaws.services.glue.util.GlueArgParser
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.functions.{col, udf}
 
 import scala.collection.JavaConverters._
 
 /**
- * An example that includes all necessary boilerplate code for a deployable   *
- * AWS Glue job script in Scala that can also be tested locally with          *
- * scalatest and sbt.
- */
+  * An example that includes all necessary boilerplate code for a deployable   *
+  * AWS Glue job script in Scala that can also be tested locally with          *
+  * scalatest and sbt. */
 object GlueApp {
 
   def main(sysArgs: Array[String]): Unit = {
 
     // Load the job arguments. "JOB_NAME" is a special argument that gets autofilled
-    val args = GlueArgParser.getResolvedOptions(
-      sysArgs,
-      Seq(
-        "JOB_NAME", "stage"
-      ).toArray)
+    val args = GlueArgParser.getResolvedOptions(sysArgs, Seq("JOB_NAME", "stage").toArray)
 
     println("Initializing Spark and GlueContext")
-    val sparkSession: SparkSession = if (args("stage") == "dev") {
-      // For testing, we need to use local execution. You need Java JDK 8 for this to work!
-      SparkSession.builder().master("local[*]").getOrCreate()
-    } else {
-      SparkSession.builder().getOrCreate()
-    }
+    val sparkSession: SparkSession =
+      if (args("stage") == "dev") { // For testing, we need to use local execution. You need Java JDK 8 for this to work!
+        SparkSession.builder().master("local[*]").getOrCreate()
+      } else {
+        SparkSession.builder().getOrCreate()
+      }
     val sparkContext: SparkContext = sparkSession.sparkContext
     sparkContext.setLogLevel("FATAL") // Can be changed to INFO, ERROR, or WARN
     val glueContext: GlueContext = new GlueContext(sparkContext)
-
 
     // Check the custom argument 'stage' to ensure if the execution is local or not
     if (args("stage") == "prod" || args("stage") == "staging") {
       Job.init(if (args("JOB_NAME") != null) args("JOB_NAME") else "test", glueContext, args.asJava)
     }
-   
+
+    val getFiguresCountUdf: UserDefinedFunction = udf((cards: String) => getFiguresCount(cards))
+    val getSuitsWithOverFiveCardsCountUdf: UserDefinedFunction = udf(
+      (cards: String) => getSuitsWithOverFiveCardsCount(cards))
+
+    // Read JSON data from S3 and load it into a DataFrame
+    var jsonDataFrame = sparkSession.read
+      .json("s3://tarnib-analytics-bucket/raw_json_data/tarnib-analytics.json")
+      .where(col("team_1_score") + col("team_2_score") === 13) // Only keep rounds that were played entirely
+      .withColumn("player_1_figures_count", getFiguresCountUdf(col("player_1_cards")))
+      .withColumn("player_2_figures_count", getFiguresCountUdf(col("player_2_cards")))
+      .withColumn("player_3_figures_count", getFiguresCountUdf(col("player_3_cards")))
+      .withColumn("player_4_figures_count", getFiguresCountUdf(col("player_4_cards")))
+      .withColumn("player_1_suits_over_five_cards_count", getSuitsWithOverFiveCardsCountUdf(col("player_1_cards")))
+      .withColumn("player_2_suits_over_five_cards_count", getSuitsWithOverFiveCardsCountUdf(col("player_2_cards")))
+      .withColumn("player_3_suits_over_five_cards_count", getSuitsWithOverFiveCardsCountUdf(col("player_3_cards")))
+      .withColumn("player_4_suits_over_five_cards_count", getSuitsWithOverFiveCardsCountUdf(col("player_4_cards")))
+
+    jsonDataFrame.show(10)
+
     println("Your code goes here!")
 
     // Job actions should only happen when executed by AWS Glue, so we ensure correct stage
@@ -50,4 +65,24 @@ object GlueApp {
     }
 
   }
+
+  /**
+    * Count occurrence of figures (Ace included) in a players cards
+    * @param cards - cards array string
+    */
+  def getFiguresCount(cards: String): Int = {
+    val occurrences = Seq("14", "13", "12", "11")
+    occurrences.map(card => card.r.findAllIn(cards).length).sum
+  }
+
+  /**
+    * Count occurrence of suits with more than 5 cards
+    * Ex: if you had 6 spades, 6 hearts, 1 diamond, the function will return 2
+    * @param cards - cards array string
+    */
+  def getSuitsWithOverFiveCardsCount(cards: String): Int = {
+    val occurrences = Seq("of_spades", "of_hearts", "of_clubs", "of_diamonds")
+    occurrences.map(card => if (card.r.findAllIn(cards).length > 5) 1 else 0).sum
+  }
+
 }
--- src/test/scala/GlueAppSpec.scala
+++ src/test/scala/GlueAppSpec.scala
@@ -1,10 +1,11 @@
+import com.fares.AWSGlue.GlueApp
 import org.scalatest.flatspec.AnyFlatSpec
 
 /**
- * A class that runs a local execution of an AWS Glue job within a scalatest
- * Instead of running our local executions, it is preferred to call them from
- * a test framework, where we are able to add assertions for verification.
- */
+  * A class that runs a local execution of an AWS Glue job within a scalatest
+  * Instead of running our local executions, it is preferred to call them from
+  * a test framework, where we are able to add assertions for verification.
+  */
 class GlueAppSpec extends AnyFlatSpec {
 
   "GlueApp" should "run successfully" in {
@@ -12,11 +13,33 @@ class GlueAppSpec extends AnyFlatSpec {
     println("Executing test case!")
     // Trigger the execution by directly calling the main class and supplying
     // arguments. AWS Glue job arguments always begin with "--"
-    com.fares.AWSGlue.GlueApp.main(Array(
-      "--JOB_NAME", "job",
-      "--stage", "dev"
-    ))
+    GlueApp.main(
+      Array(
+        "--JOB_NAME",
+        "job",
+        "--stage",
+        "dev"
+      ))
 
     assert(true)
+  }
+
+
+  it should "count the correct amount of figures" in {
+    val player_cards =
+      "[\"10_of_hearts\",\"2_of_hearts\",\"14_of_spades\",\"13_of_spades\",\"7_of_spades\",\"2_of_spades\"," +
+        "\"14_of_diamonds\",\"10_of_diamonds\",\"8_of_diamonds\",\"3_of_diamonds\",\"14_of_clubs\",\"12_of_clubs\"," +
+        "\"8_of_clubs\"]"
+    val figures_count = GlueApp.getFiguresCount(player_cards)
+    assert(figures_count == 5)
+  }
+
+  it should "count the correct amount of suits with over 5 cards" in {
+    val player_cards =
+      "[\"10_of_hearts\",\"2_of_hearts\",\"14_of_spades\",\"13_of_spades\",\"14_of_diamonds\",\"13_of_diamonds\"" +
+        "\"12_of_diamonds\",\"10_of_diamonds\",\"8_of_diamonds\",\"3_of_diamonds\",\"14_of_clubs\",\"12_of_clubs\"," +
+        "\"8_of_clubs\"]"
+    val suits_with_over_five_cards_count = GlueApp.getSuitsWithOverFiveCardsCount(player_cards)
+    assert(suits_with_over_five_cards_count == 1)
   }
 }
